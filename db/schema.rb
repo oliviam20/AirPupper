b5b71575f6a9f4f1022b8571c6c27071b07dfa4d
Olivia Mo <oliviatrademark@gmail.com>
2017-11-02 15:24:09 +1100

    migrate dog model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171102024542) do
+ActiveRecord::Schema.define(version: 20171102041451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "age"
+    t.boolean "tricks"
+    t.boolean "kid_friendly"
+    t.boolean "obedient"
+    t.boolean "energetic"
+    t.boolean "small"
+    t.boolean "medium"
+    t.boolean "large"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
