dbf89934d6bceca04e84a21529571174cab3857c
Olivia Mo <oliviatrademark@gmail.com>
2017-11-02 15:44:05 +1100

    added @dog.user = current_user to controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :dogs
-  root 'pages#index'
+  root 'dogs#index'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
