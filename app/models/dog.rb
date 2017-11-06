d9a4bbfe24de331c1b9251d261602e3ae54dc78f
Olivia Mo <oliviatrademark@gmail.com>
2017-11-06 16:05:28 +1100

    added description search

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -4,7 +4,7 @@ class Dog < ApplicationRecord
 
   def self.search(term)
     if term
-      where('name ILIKE ?', "%#{term}%").order('id DESC')
+      where('name ILIKE ? OR description ILIKE ?', "%#{term}%", "%#{term}%").order('id DESC')
     else
       order('id DESC')
     end
