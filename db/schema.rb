1da1099b220bead7231e24d5eba242d84f386f56
Olivia Mo <oliviatrademark@gmail.com>
2017-11-07 17:34:38 +1100

    added geocoder to dog and can show map on show page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106052949) do
+ActiveRecord::Schema.define(version: 20171107060158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,26 @@ ActiveRecord::Schema.define(version: 20171106052949) do
     t.string "image"
     t.float "price"
     t.text "breed"
+    t.text "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_dogs_on_user_id"
+  end
+
+  create_table "downloads", force: :cascade do |t|
+    t.string "ip"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "messages", force: :cascade do |t|
